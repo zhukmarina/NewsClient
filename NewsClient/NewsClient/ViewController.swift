9a5776d6fb8a67793c04fcd5844142c593c7416b
Marina Zhukova <marinazhukova28@gmail.com>
2024-06-07 16:14:33 +0300

    initial commit

---
+++ NewsClient/NewsClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsClient
+//
+//  Created by Marina Zhukova on 07.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
